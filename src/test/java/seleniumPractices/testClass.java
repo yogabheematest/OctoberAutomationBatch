fc1bbc5a04d20cd92a068f5ec9fda8514d15fc20
nelloruseshu <poojya2014@gmail.com>
2022-11-10 18:44:20 +0530

    seshu commits

---
+++ src/test/java/seleniumPractices/testClass.java
@@ -0,0 +1,15 @@
+package seleniumPractices;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+	
+	@Test
+	public void Method1()
+	{
+		System.out.println("Method1");
+	}
+	
+	
+
+}
